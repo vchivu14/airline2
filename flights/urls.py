457b87f1debe7f33af2443cc06c5cd94a21764d2
vchivu <chivu.vlad@yahoo.com>
2020-05-08 16:18:06 +0300

    remove passenger from flight

--- flights/urls.py
+++ flights/urls.py
@@ -4,8 +4,12 @@ from . import views
 app_name = 'flights'
 urlpatterns = [
     path("", views.index, name="index"),
+    path("addairport/", views.addairport, name="addairport"),
     path("addflight/", views.addflight, name="addflight"),
+    path("addpassenger/", views.addpassenger, name="addpassenger"),
+    path("deletepassenger/", views.deletepassenger, name="deletepassenger"),
     path("<int:flight_id>/", views.flight, name="flight"),
-    path("<int:flight_id>/book/", views.signup, name="book"),
+    path("<int:flight_id>/book/", views.book, name="book"),
+    path("<int:flight_id>/deletebooking/", views.deletebooking, name="deletebook"),
     path("<int:flight_id>/signup/", views.signup, name="signup"),
 ]
--- flights/views.py
+++ flights/views.py
@@ -8,7 +8,8 @@ from django.db.models import F
 def index(request):
     context = {
         "flights": Flight.objects.all(),
-        "airports": Airport.objects.all()
+        "airports": Airport.objects.all(),
+        "passengers": Passenger.objects.all()
     }
     return render(request, "flights/index.html", context)
 
@@ -21,13 +22,13 @@ def flight(request, flight_id):
     context = {
         "flight": flight,
         "passengers": flight.passengers.all(),
-        "non_passengers": Passenger.objects.exclude(flights=flight).all(),
+        "non_passengers": Passenger.objects.exclude(flights=flight).all()
     }
     return render(request, "flights/flight.html", context)
 
 def book(request, flight_id):
     seats = Flight.objects.get(pk=flight_id).seats
-    if seats > 1:
+    if seats >= 1:
         try:
             passenger_id = int(request.POST["passenger"])
             flight = Flight.objects.get(pk=flight_id)
@@ -45,6 +46,27 @@ def book(request, flight_id):
     else:
         return HttpResponse("No seats available")
     
+def deletebooking(request, flight_id):
+    seats = Flight.objects.get(pk=flight_id).seats
+    passengers = Flight.objects.get(pk=flight_id).passengers.all()
+    if passengers:
+        try:
+            passenger_id = int(request.POST["passenger"])
+            flight = Flight.objects.get(pk=flight_id)
+            passenger = Passenger.objects.get(pk=passenger_id)
+        except KeyError:
+            return render(request, "flights/error.html", {"message": "No selection."})
+        except Flight.DoesNotExist:
+            return render(request, "flights/error.html", {"message": "No Flight."})
+        except Passenger.DoesNotExist:
+            return render(request, "flights/error.html", {"message": "No passenger."})
+        passenger.flights.remove(flight)
+        flight.seats = F('seats') + 1
+        flight.save()
+        return HttpResponseRedirect(reverse("flights:flight", args=(flight_id,)))
+    else:
+        return HttpResponse("Plane is empty")
+    
 def signup(request, flight_id):
     seats = Flight.objects.get(pk=flight_id).seats
     if seats > 1:
@@ -53,7 +75,7 @@ def signup(request, flight_id):
             last_name = request.POST["last"]
             flight = Flight.objects.get(pk=flight_id)
         except KeyError:
-            return render(request, "flights/error.html", {"message": "No selection."})
+            return render(request, "flights/error.html", {"message": "No entries."})
         passenger = Passenger(first=first_name, last=last_name)
         passenger.save()
         passenger.flights.add(flight)
@@ -76,4 +98,32 @@ def addflight(request):
     flight = Flight(origin=a_origin, destination=a_destination, duration=duration, seats=seats)
     flight.save()
     return HttpResponseRedirect(reverse("flights:index"))
-        
+
+def addairport(request):
+    try:
+        name = request.POST["city"]
+        code = request.POST["code"]
+    except KeyError:
+        return render(request, "flights/error.html", {"message": "No entries."})
+    airport = Airport(city=name, code=code)
+    airport.save()
+    return HttpResponseRedirect(reverse("flights:index"))
+
+def addpassenger(request):
+    try:
+        first = request.POST["first"]
+        last = request.POST["last"]
+    except KeyError:
+        return render(request, "flights/error.html", {"message": "No entries."})
+    passenger = Passenger(first=first, last=last)
+    passenger.save()
+    return HttpResponseRedirect(reverse("flights:index"))
+
+def deletepassenger(request):
+    try:
+        passenger_id = int(request.POST["passenger"])
+        passenger = Passenger.objects.get(pk=passenger_id)
+    except KeyError:
+        return render(request, "flights/error.html", {"message": "No entries."})
+    passenger.delete()
+    return HttpResponseRedirect(reverse("flights:index"))
